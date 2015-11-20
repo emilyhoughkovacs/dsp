9c84c0a8222a80e4c0725aac3e34984dfcafe45e
Emily Hough-Kovacs <emilyhoughkovacs@gmail.com>
2015-11-19 21:33:27 -0500

    part II adv python, write to csv

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,9 @@
-PLACE YOUR CODE HERE
+import csv
+
+with open('faculty.csv', 'rb') as faculty:
+    with open('emails.csv', 'wb') as emails:
+        fy = csv.reader(faculty)
+        wr = csv.writer(emails)
+        header = fy.next()
+        for name, degree, title, email in fy:
+            wr.writerow([email])
